79e50f4b1633c371946aff7df92d09afdc81eee9
akshat02 <akshatt94@gmail.com>
2019-09-15 23:58:43 +0530

    Updated amount, place and date
    Updated to scrape amount, place and date of transaction from snippet

--- readCitiTransacs.py
+++ readCitiTransacs.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import authorize as au
+import re
+from datetime import datetime
 
 def get_messages(service):
     citiTransacsLabelID = 'Label_3370579833892165018'
@@ -25,12 +27,33 @@ def main():
     else:
         for message in messages:
             message_content = service.users().messages().get(userId = 'me', id = message['id']).execute()
-            data.append([message_content['id'], message_content['payload']['headers'][7]['value'], message_content['payload']['headers'][26]['value'], message_content['snippet']])
 
-    mails_df = pd.DataFrame(data, columns = ['Message Id', 'From', 'Subject', 'Snippet'])
+            snippet = message_content['snippet']
 
-    fileName = 'Citi_Gmail.xlsx'
-    mails_df.to_excel(fileName)
+            cost_match = re.search(r'\d+[.]\d+', snippet)
+            if cost_match:
+                cost = cost_match.group()
+                cost = float(cost)
+            else:
+                cost = 'could not find'
+
+            place_match = re.search(r'at\s[\w\s\/\,\*.]+\sLimit', snippet)
+            if place_match:
+                place = place_match.group()[3:-7]
+                # place = place[3:-7]
+            else:
+                place = 'could not find'
+
+            datetime_string = message_content['payload']['headers'][22]['value']
+            dateTime = datetime.strptime(datetime_string[:-18], '%a, %d %b %Y %H:%M:%S')
+
+            data.append([dateTime, cost, place, snippet])
+
+    mails_df = pd.DataFrame(data, columns = ['Date', 'Amount', 'Place', 'Snippet'])
+
+    fileName = 'Citi_Gmail.csv'
+    # mails_df.to_excel(fileName)
+    mails_df.to_csv(fileName)
 
     print('File Ready')
 
